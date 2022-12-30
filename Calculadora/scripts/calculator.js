9934183940c103dc612b3dfb87b8df3317032310
Henrique Moraes <ahmdesouza@gmail.com>
2022-12-30 00:27:00 -0300

    update

--- Calculadora/scripts/calculator.js
+++ Calculadora/scripts/calculator.js
@@ -1,1 +1,1 @@
-let calculadora = new CalcController();
+let calculator = new CalcController();
--- Calculadora/scripts/controller/calcController.js
+++ Calculadora/scripts/controller/calcController.js
@@ -3,11 +3,18 @@ class CalcController {
     constructor(){
         this._displayCalc = '0';
         this._currentDate;
-        this.initialize
+        this.initialize();
     }
 
     initialize(){
 
+        let displayCalcEl = document.querySelector('#display');
+        let dateEl = document.querySelector('#data');
+        let timeEl = document.querySelector('#hora');
+        
+        dateEl.innerHTML = '4/5/67'
+        displayCalcEl.innerHTML = '4567'
+        timeEl.innerHTML = '45:673'
     }
 
     get displayCalc(){
