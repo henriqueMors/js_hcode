bb91f6dd112da99f09f681e990504f88d858c94d
Henrique Moraes <ahmdesouza@gmail.com>
2022-12-31 14:47:32 -0300

    Update calcController.js

--- Calculadora/scripts/controller/calcController.js
+++ Calculadora/scripts/controller/calcController.js
@@ -60,29 +60,64 @@ class CalcController {
 
     }
 
+    pushOperation(value){
+
+        this._operation.push(value);
+
+        if (this._operation.length > 3) {
+
+            this.calc();
+
+        }
+
+    }
+
+    calc(){
+
+        let last = this._operation.pop();
+        let result = eval(this._operation.join(''));
+        this._operation = [result, last];
+
+    }
+
+    setLastNumberToDisplay(){
+
+        
+    }
+
     addOperation(value){
 
         if (isNaN(this.getLastOperation())) {
 
             if (this.isOperator(value)) {
+
                 this.setLastOperation(value);
 
             } else if (isNaN(value)) {
 
+                // add valor
 
             } else {
                 
-                this._operation.push(value);
-                
+                this.pushOperation(value);
                 
             }
 
         } else {
-            let newValue = thi.getLastOperation().toString() + value.toString();
-            this.setLastOperation(parseInt(newValue));
-        }
 
-        
+            if (this.isOperator(value)) {
+
+                this.pushOperation(value);
+
+            } else {
+
+                let newValue = this.getLastOperation().toString() + value.toString();
+                this.setLastOperation(parseInt(newValue));
+                this.setLastNumberToDisplay();
+
+            }
+
+        }
 
     }
 
@@ -148,10 +183,10 @@ class CalcController {
             default:
                 this.setError();
                 break;
-
             
         }
     }
+
     initButtonsEvents(){
 
         let buttons = document.querySelectorAll('#buttons > g, #parts > g');
