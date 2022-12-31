e36efb8607337d6aac91a9fd70e842f627fcd850
Henrique Moraes <ahmdesouza@gmail.com>
2022-12-31 13:37:06 -0300

    Update calcController.js

--- Calculadora/scripts/controller/calcController.js
+++ Calculadora/scripts/controller/calcController.js
@@ -1,7 +1,7 @@
 class CalcController {
 
     constructor(){
-
+        this._operation = [];
         this._locale = 'pt-BR';
         this._displayCalcEl = document.querySelector('#display');
         this._dateEl = document.querySelector('#date');
@@ -30,13 +30,110 @@ class CalcController {
         })
     }
 
+    clearAll(){
+
+        thi._operation = [];
+
+    }
+
+    clearEntry(){
+
+        this._operation.pop();
+
+    }
+
+    getLastOperation() {
+
+        return this._operation[this._operation.length - 1];
+
+    }
+
+    addOperation(value){
+
+        if (isNaN(thi.getLastOperation())) {
+
+        } else {
+            thi.getLastOperation().toString() + value.toString()
+        }
+
+        this._operation.push(value);
+
+    }
+
+    setError(){
+
+        this.displayCalc = 'ERROR';
+
+    }
+
+    execBtn(value) {
+            console.log(value)
+        switch (value) {
+
+            case 'ac':
+                this.clearAll();
+                break;
+
+            case 'ce':
+                this.clearEntry();
+                break;
+
+            case 'soma':
+
+                break;
+
+            case 'subtracao':
+
+                break;
+
+            case 'divisao':
+
+                break;
+
+            case 'multiplicacao':
+
+                break;
+
+            case 'porcento':
+
+                break;
+
+            case 'igual':
+
+                break;
+
+            case 'ponto':
+
+                break;
+
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+                this.addOperation(parseInt(value));
+                break;
+
+            default:
+                this.setError();
+                break;
+
+            
+        }
+    }
     initButtonsEvents(){
 
         let buttons = document.querySelectorAll('#buttons > g, #parts > g');
             
             buttons.forEach((btn, index) => {
-                this.addEventListenerAll(btn, 'click drag mouseover', e => {
-                    console.log(btn.className.baseVal.replace('btn-',''));
+                this.addEventListenerAll(btn, 'click drag', e => {
+                    let textBtn = btn.className.baseVal.replace('btn-','');
+                    this.execBtn(textBtn);
             });
 
             this.addEventListenerAll(btn, 'mouseover mouseup mousedown', e => {
