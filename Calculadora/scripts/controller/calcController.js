17d172170ac04f4ec0fa078f74641db1508aa61c
Henrique Moraes <ahmdesouza@gmail.com>
2023-01-01 21:44:05 -0300

    Update calcController.js

--- Calculadora/scripts/controller/calcController.js
+++ Calculadora/scripts/controller/calcController.js
@@ -179,9 +179,14 @@ class CalcController {
     }
 
     getResult(){
-
+        try{
         return eval(this._operation.join(''));
+        } catch (e) {
+            setTimeout(()=>{
+                this.setError();
+            }, 1);
 
+        }
     }
 
     calc(){
