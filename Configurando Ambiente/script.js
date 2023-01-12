ac000d87ab08dcb5208257ac428607a9bf36513f
Henrique Moraes <ahmdesouza@gmail.com>
2023-01-12 14:00:11 -0300

    UPDATE

---
+++ Configurando Ambiente/script.js
@@ -0,0 +1,3 @@
+var nome = document.querySelector("#exampleInputName");
+nome.value = "fuck off"; // A VARIAVEL NOME VIRA UMA REFERENCIA DE OBJETO
+nome.style.color = "red" // MANIPULA O ELEMENTO COR DO OBJETO
