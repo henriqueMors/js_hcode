a0222973ef1c5c96aa853d9e2aa920a8b8b081be
Henrique Moraes <ahmdesouza@gmail.com>
2023-02-08 19:22:47 -0300

    Update script.js

--- Configurando Ambiente/script.js
+++ Configurando Ambiente/script.js
@@ -5,14 +5,23 @@ nome.style.color = "#ccc" // MANIPULA A APARENCIA DO OBJETO
 var fields = document.querySelectorAll('#form-user-create [name]')
 var user = {};
 
-fields.forEach(function(field, index){ //forEach PERCORRE CAMPOS DE UM OBJETO / EXECUTAR UMA FUNCAO PARA CADA ITEM
 
-    if (field.name == 'gender') {
 
-        if (field.checked) {
+console.log(user);
+
+document.getElementById("form-user-create").addEventListener("submit", function(event){
+    event.preventDefault();
+
+    fields.forEach(function(field, index){ //forEach PERCORRE CAMPOS DE UM OBJETO / EXECUTAR UMA FUNCAO PARA CADA ITEM
+
+        if (field.name == 'gender') {
+    
+            if (field.checked) {
+                user[field.name] = field.value;
+            }
+        } else {
             user[field.name] = field.value;
         }
-    } else {
-        user[field.name] = field.value;
-    }
+    });
+
 });
