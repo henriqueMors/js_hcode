662fe8800cf291363c82b4012910a7c91326b259
Henrique Moraes <ahmdesouza@gmail.com>
2023-02-08 14:03:27 -0300

    update

--- Configurando Ambiente/script.js
+++ Configurando Ambiente/script.js
@@ -1,6 +1,6 @@
 var nome = document.querySelector("#exampleInputName");
 nome.value = "INSERT NAME"; // A VARIAVEL NOME VIRA UMA REFERENCIA DE OBJETO
-nome.style.color = "red" // MANIPULA A APARENCIA DO OBJETO
+nome.style.color = "#ccc" // MANIPULA A APARENCIA DO OBJETO
 
 var gender = document.querySelectorAll('#form-user-create [name=gender]:checked') // UTILIZANDO UM METODO SELETOR / INPUT
 var birth = document.querySelector('#exampleInputBirth')
@@ -11,14 +11,18 @@ var photo = document.querySelector('#exampleInputFile')
 var admin = document.querySelector('#exampleInputAdmin')
 
 var fields = document.querySelectorAll('#form-user-create [name]')
+var user = {};
+
 fields.forEach(function(field, index){ //forEach PERCORRE CAMPOS DE UM OBJETO / EXECUTAR UMA FUNCAO PARA CADA ITEM
 
     if (field.name == 'gender') {
 
         if (field.checked) {
-            console.log('sim', field);
+            user.gender
         }
     } else {
-        console.log('nao')
+        user[field.name] = field.value;
     }
 });
+
+console.log(user);
