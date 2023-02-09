15fe877fa60f4ca87b64ac507939860cf2997242
Henrique Moraes <ahmdesouza@gmail.com>
2023-02-09 13:57:13 -0300

    updartew

--- Configurando Ambiente/script.js
+++ Configurando Ambiente/script.js
@@ -40,6 +40,17 @@ document.getElementById("form-user-create").addEventListener("submit", function(
         }
     });
 
-    addLine(user);
+    var objectUser = new User(
+                                user.name, 
+                                user.gender, 
+                                user.country, 
+                                user.birth, 
+                                user.email, 
+                                user.password, 
+                                user.photo, 
+                                user.admin
+                            );
+
+    addLine(objectUser);
 
 });
