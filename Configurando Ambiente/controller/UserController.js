d1438306f2b679298c0cda71d83c7dfe2b9806bb
Henrique Moraes <ahmdesouza@gmail.com>
2023-02-10 14:58:02 -0300

    Update UserController.js

--- Configurando Ambiente/controller/UserController.js
+++ Configurando Ambiente/controller/UserController.js
@@ -1,8 +1,38 @@
 class UserController {
 
-    constructor() {
+    constructor(formId) {
 
+        this.formEl = document.getElementById(formId);
+
+
+    }
+
+    getValues() {
+
+        fields.forEach(function(field, index){ //forEach PERCORRE CAMPOS DE UM OBJETO / EXECUTAR UMA FUNCAO PARA CADA ITEM
+
+            if (field.name == 'gender') {
         
+                if (field.checked) {
+                    user[field.name] = field.value;
+                }
+            } else {
+                user[field.name] = field.value;
+            }
+        });
+    
+        var objectUser = new User(
+                                    user.name, 
+                                    user.gender, 
+                                    user.country, 
+                                    user.birth, 
+                                    user.email, 
+                                    user.password, 
+                                    user.photo, 
+                                    user.admin
+                                );
+    
+        addLine(objectUser);
 
     }
 
