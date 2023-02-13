e9eab42b52e7302879d3f58345224f4c0dc29d2e
Henrique Moraes <ahmdesouza@gmail.com>
2023-02-13 15:28:17 -0300

    update

--- Configurando Ambiente/controller/UserController.js
+++ Configurando Ambiente/controller/UserController.js
@@ -1,9 +1,11 @@
 class UserController {
 
-    constructor(formId) {
+    constructor(formId, tableId) {
 
         this.formEl = document.getElementById(formId);
+        this.tableEl = document.getElementById(tableId);
 
+        this.onSubmit();
 
     }
 
@@ -14,7 +16,7 @@ class UserController {
     
             event.preventDefault();
 
-            this.getValues();
+            this.addLine(this.getValues())
           
         });        
     }
@@ -45,6 +47,24 @@ class UserController {
                         user.photo, 
                         user.admin
                         );
+    }
+
+    addLine(dataUser) {
+
+    
+            this.tableEl.innerHTML = 
+                `<tr>
+                    <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
+                    <td>${dataUser.name}</td>
+                    <td>${dataUser.email}</td>
+                    <td>${dataUser.admin}</td>
+                    <td>${dataUser.birth}</td>
+                    <td>
+                    <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+                    <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+                    </td>
+                </tr>`;
+    
     }
 
 }
